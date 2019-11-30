e6a2da9ab8ef3507cd33ea4287281226281b2bc3
Ivan <issibirtsev@gmail.com>
2019-11-30 15:19:08 +0300

    0.1

---
+++ app/src/main/java/com/example/hard/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.hard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onRegisterButtonClick(View v){
+        Intent i = new Intent(MainActivity.this, )
+    }
+    public void OnEnterButtonClick(View v){
+        String login = ((EditText) findViewById(R.id.log)).toString();
+        String password = ((EditText)findViewById(R.id.pass)).toString();
+    }
+}
---
+++ app/src/main/java/com/example/hard/Register_activity.java
@@ -0,0 +1,14 @@
+package com.example.hard;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
